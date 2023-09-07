72fac796e6e401e15d166a6b0ddb2eb4f33eda26
jhargett1 <119984652+jhargett1@users.noreply.github.com>
2023-09-07 17:08:48 -0500

    Salting and hashing password with bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const secret = process.env.SECRET;
 
@@ -37,52 +39,50 @@ app.get("/register", async (req, res) => {
 });
 
 app.post("/register", async  (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save()
-    .then(createdUser => {
-        if(createdUser) {
-            res.render("secrets");
-        } else {
-            console.log("Something wrong happened.");
-            res.redirect("/");
-        }
-    
-    })
-    .catch(err => {
-        console.log(err);
-        res.redirect("/");
-    });
-});
 
-app.post("/login", async (req, res) => {
-    const username = req.body.username;
-    const password = md5(req.body.password);
-
-    await User.findOne({email: username})
-    .then(foundUser => {
-        if(foundUser) {
-            if (foundUser.password === password) {
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save()
+        .then(createdUser => {
+            if(createdUser) {
                 res.render("secrets");
-                console.log(foundUser.password);
             } else {
-                console.log("Incorrect password.");
+                console.log("Something wrong happened.");
                 res.redirect("/");
             }
-        } else {
-            console.log("Username not found.");
+        
+        })
+        .catch(err => {
+            console.log(err);
             res.redirect("/");
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.redirect("/");
+        });
     });
 
+    
 });
 
+app.post("/login", async (req, res) => { 
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    try {
+      const user = await User.findOne({email: username});
+  
+      if (user) {
+        const result = bcrypt.compare(password, user.password);
+        
+        if (result) {
+          res.render("secrets");
+        } 
+      }
+  
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
 app.listen(3000, () => {
     console.log("Server started on port 3000.")
